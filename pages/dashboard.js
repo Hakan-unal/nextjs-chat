d222ab890def2b4a07f91d80d8278e8e511ea7bd
Con Sınov <hakanunal@devtagon.com>
2022-06-15 22:59:40 +0300

    updated

---
+++ pages/dashboard.js
@@ -0,0 +1,31 @@
+
+
+
+
+const Dashboard = ({ datas }) => {
+    return (
+        <div>
+            <h1>Datas</h1>
+            {datas.map((data) => {
+                <div key={data.id}>
+                    merhaba
+                    BU BİR DENEM
+                </div>
+
+            })}
+        </div>
+    )
+}
+
+export default Dashboard
+
+
+export const getStaticProps = async () => {
+    const res = await fetch("https://jsonplaceholder.typicode.com/users");
+    const data = await res.json()
+
+    console.log(data)
+    return {
+        props: { datas: [{ id: 1 }, { id: 2 }] }
+    }
+}
